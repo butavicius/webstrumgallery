485188f2c52477ad540ad9e9cf779b46fbc32140
Simas Butavičius <simasbutavicius@gmail.com>
2022-03-28 03:01:28 +0300

    plugin version

--- webstrumgallery.php
+++ webstrumgallery.php
@@ -43,7 +43,7 @@ class WebstrumGallery extends Module
     {
         $this->name = 'webstrumgallery';
         $this->tab = 'front_office_features';
-        $this->version = '1.0.0';
+        $this->version = '0.1.0';
         $this->author = 'Simas Butavičius';
         $this->need_instance = 0;
         $this->bootstrap = true;
